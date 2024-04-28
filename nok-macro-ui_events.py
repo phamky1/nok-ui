cd457a37c9eea0d92041e7d1cacd79f21fdbe182
Your Name <you@example.com>
2024-04-28 17:25:52 +0700

    update feature pomodoro todo

---
+++ nok-macro-ui_events.py
@@ -0,0 +1,32 @@
+
+def helloworld(event_struct):
+    return
+
+
+def loadTaskInfo(event_struct):
+    return
+
+
+def updateTaskList(event_struct):
+    return
+
+
+def loadListTask(event_struct):
+    return
+
+
+def loadPomodoroTimer(event_struct):
+    return
+
+
+def updatePomodoroTimer(event_struct):
+    return
+
+
+def pausePmdrTimer(event_struct):
+    return
+
+
+def stopPmdrTimer(event_struct):
+    return
+
